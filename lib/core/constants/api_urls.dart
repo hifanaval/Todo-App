8a976921ec51090b64c506d136b2ce7943b78570
Hifa <hifanaval214@gmail.com>
2025-12-15 17:59:13 +0530

    app icon added

--- lib/core/constants/api_urls.dart
+++ lib/core/constants/api_urls.dart
@@ -1,8 +1,5 @@
 class ApiUrls {
   static const todos = 'https://jsonplaceholder.typicode.com/todos';
   static const users = 'https://jsonplaceholder.typicode.com/users';
-  // For profile, we'll use a mock endpoint or extend users endpoint
-  // In production, replace with your actual profile API endpoint
-  static String profileByEmail(String email) => 'https://jsonplaceholder.typicode.com/users?email=$email';
-  static String updateProfile(int id) => 'https://jsonplaceholder.typicode.com/users/$id';
+
 }
